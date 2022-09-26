cd9288930c9f923a288123bcddf62738692c07ef
LoiolaBr17 <mateustaua16@gmail.com>
2022-09-26 11:14:40 -0300

    add tasks

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,23 @@ const App = () => {
     {id: 2, name: 'Comprar massa de bolo', done: true}
   ]);
 
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+    });
+
+    setList(newList);
+  }
+
   return (
     <C.Container>
       <C.Area>
         <C.Header>Lista de Tarefas</C.Header>
 
-        <AddArea />
+        <AddArea onEnter={handleAddTask} />
 
         {list.map((item, index) => (
           <ListItem 
--- src/components/AddArea/index.tsx
+++ src/components/AddArea/index.tsx
@@ -1,6 +1,19 @@
+import { useState, KeyboardEvent } from 'react';
 import * as C from './styles';
 
-const AddArea = () => {
+type Props = {
+    onEnter: (taskName: string) => void
+}
+
+const AddArea = ({ onEnter }: Props ) => {
+    const [inputText, setInputText] = useState('')
+
+    const handleKeyUp = (e: KeyboardEvent) => {
+       if(e.code === 'Enter' && inputText !== ''){
+            onEnter(inputText);
+            setInputText('');
+       }
+    }
 
     return (
         <C.Container>
@@ -8,6 +21,9 @@ const AddArea = () => {
             <input 
                 type="text" 
                 placeholder="Adicione uma tarefa"
+                value={inputText}
+                onChange={e => setInputText(e.target.value)}
+                onKeyUp={handleKeyUp}
             />
         </C.Container>
     )
